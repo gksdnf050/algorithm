8ff92d4125f52aabe52bc3d9dff972c036e16d4e
hanul <gksdnf050@naver.com>
2021-09-26 16:25:33 +0900

    [BaekJoon] 9012

---
+++ src/main/java/com/example/study/algorithm/baekjoon/BaekJoon_9012.java
@@ -0,0 +1,54 @@
+package com.example.study.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BaekJoon_9012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++) {
+            String str = sc.next();
+            check(str);
+        }
+
+
+    }
+
+    private static void check(String str) {
+        if(str.charAt(0) == ')') {
+            System.out.println("NO");
+            return;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(c == '(') {
+               stack.push(c);
+           }
+
+           if(c == ')') {
+               if(stack.size() == 0) {
+                   System.out.println("NO");
+                   return;
+               } else {
+                   stack.pop();
+               }
+           }
+        }
+
+        if(stack.size() == 0) {
+            System.out.println("YES");
+            return;
+        }
+
+        System.out.println("NO");
+    }
+
+}

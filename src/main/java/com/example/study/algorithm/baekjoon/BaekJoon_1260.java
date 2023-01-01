ce0b7a65dd31f5206b37f6218b36233e55417be2
201411146 <gksdnf050@naver.com>
2023-01-02 00:57:45 +0900

    [baekjoon] 1260-DFS와 BFS (2)

--- src/main/java/com/example/study/algorithm/baekjoon/BaekJoon_1260.java
+++ src/main/java/com/example/study/algorithm/baekjoon/BaekJoon_1260.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BaekJoon_1260 {
@@ -12,8 +13,8 @@ public class BaekJoon_1260 {
     private static int M;
     private static int V;
     private static int[][] graph;
-    private static boolean[] bfs_visit;
-    private static boolean[] dfs_visit;
+    private static boolean[] bfsVisit;
+    private static boolean[] dfsVisit;
 
     public static void main(String[] args) throws IOException {
 
@@ -25,18 +26,18 @@ public class BaekJoon_1260 {
         V = Integer.parseInt(st.nextToken());
 
         graph = new int[N + 1][N + 1];
-        bfs_visit = new boolean[N + 1];
-        dfs_visit = new boolean[N + 1];
+        bfsVisit = new boolean[N + 1];
+        dfsVisit = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()); // TODO memory address check
+            int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             graph[x][y] = 1;
             graph[y][x] = 1;
         }
 
-        dfs(V);
+        dfsWithStack(V);
         System.out.println("");
         bfs(V);
 
@@ -45,16 +46,16 @@ public class BaekJoon_1260 {
     private static void bfs(int node) {
         Queue<Integer> q = new LinkedList<>();
         q.add(node);
-        bfs_visit[node] = true;
+        bfsVisit[node] = true;
 
         while (q.isEmpty() == false) {
             int temp = q.poll();
             System.out.print(temp + " ");
 
             for (int i = 0; i <= N; i ++) {
-                if (bfs_visit[i] == false && graph[temp][i] == 1) {
+                if (bfsVisit[i] == false && graph[temp][i] == 1) {
                     q.add(i);
-                    bfs_visit[i] = true;
+                    bfsVisit[i] = true;
                 }
             }
         }
@@ -62,12 +63,42 @@ public class BaekJoon_1260 {
 
     private static void dfs(int node) {
         System.out.print(node + " ");
-        dfs_visit[node] = true;
+        dfsVisit[node] = true;
 
         for (int i = 0; i <= N; i++) {
-            if (dfs_visit[i] == false && graph[i][node] == 1) {
+            if (dfsVisit[i] == false && graph[node][i] == 1) {
                 dfs(i);
             }
         }
+    }
+
+    private static void dfsWithStack(int node) {
+        Stack<Integer> s = new Stack<>();
+        s.push(node);
+        dfsVisit[node] = true;
+        System.out.print(node + " ");
+
+        boolean flag = true;
+
+        while (s.isEmpty() == false) {
+            int top = s.peek();
+
+            flag = true;
+
+            for (int i = 0; i <= N; i ++) {
+                if (dfsVisit[i] == false && graph[top][i] == 1) {
+                    s.push(i);
+                    dfsVisit[i] = true;
+                    System.out.print(i + " ");
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag == true) {
+                s.pop();
+            }
+        }
+
     }
 }
